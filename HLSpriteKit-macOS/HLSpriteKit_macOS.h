80ef5d830ca93be1f5a1695ec9a9c7300267753f
Thomas Bonk <thomas@meandmymac.de>
2019-06-09 19:15:25 +0200

    added target for macOS

---
+++ HLSpriteKit-macOS/HLSpriteKit_macOS.h
@@ -0,0 +1,19 @@
+//
+//  HLSpriteKit_macOS.h
+//  HLSpriteKit-macOS
+//
+//  Created by Thomas Bonk on 09.06.19.
+//  Copyright © 2019 Hilo Games. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for HLSpriteKit_macOS.
+FOUNDATION_EXPORT double HLSpriteKit_macOSVersionNumber;
+
+//! Project version string for HLSpriteKit_macOS.
+FOUNDATION_EXPORT const unsigned char HLSpriteKit_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HLSpriteKit_macOS/PublicHeader.h>
+
+
